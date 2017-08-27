f7e34a9e7b58ebb19afcbfef4ad392e5bb30384e
hamishdickson <hamish.dickson@gmail.com>
2017-08-27 21:01:40 +0100

    add identity

--- Logic.scala
+++ Logic.scala
@@ -6,13 +6,18 @@ object Logic {
 
   // this is dodgy and should never be used - there is an assumption about excluded middles here
   // todo - think about using a topos
-  //type ¬¬[A] = ¬[¬[A]]  
+  //type ¬¬[A] = ¬[¬[A]]
+
+  def identity[A](a: A): A = a
 
   def not[A](a: A)(implicit nope: Invertable[A]): nope.¬[A] = nope.invert(a)
 
   // todo figure out how to express this type
-  // todo could use a poly and a map here
-  def cnot[A, B](ab: A :: B :: HNil)(implicit cont: Control[A], nope: Invertable[B]) =
+  def cnot[A, B](ab: A :: B :: HNil)(
+    implicit
+    cont: Control[A],
+    nope: Invertable[B]
+  ) =
     if (cont.control(ab.head)) ab.head :: nope.invert(ab.tail.head) :: HNil
     else ab.head :: ab.tail.head :: HNil
 }
